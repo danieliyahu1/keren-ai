af1141c10026e2603f0d3b31b3e8870ec90fa6d6
danieliyahu1 <danieliyahu1@gmail.com>
2026-01-25 21:56:06 +0200

    fix: auth service db tables initialization

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -10,23 +10,6 @@ CREATE TABLE IF NOT EXISTS "users" (
     created_at TIMESTAMP NOT NULL
 );
 
--- Insert the user if no existing user with the same id or email exists
-
-INSERT INTO "users" (id, user_id, email, encoded_password, provider, provider_user_id, created_at)
-SELECT '99999999-8888-7777-6666-555555555555', -- auto-generate DB PK
-       '11111111-2222-3333-4444-555555555555',
-       'dummy@example.com',
-       '$2a$12$PFBUjyADDfV3QQui42SL6OrJgzv.GwHKjYh.n./GyqGadNB6/Gjrq',
-       'LOCAL',
-       NULL,
-       now()
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM "users"
-    WHERE user_id = '11111111-2222-3333-4444-555555555555'
-       OR email = 'dummy@example.com'
-);
-
 -- Ensure the 'refresh_tokens' table exists
 CREATE TABLE IF NOT EXISTS "refresh_tokens" (
     id UUID PRIMARY KEY,
@@ -34,18 +17,4 @@ CREATE TABLE IF NOT EXISTS "refresh_tokens" (
     user_id UUID NOT NULL REFERENCES "users"(id),
     issued_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL
-);
-
--- Insert the refresh token if no existing refresh token with the same id or token exists
-INSERT INTO "refresh_tokens" (id, token, user_id, issued_at, expires_at)
-SELECT 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee',
-       'sample-refresh-token-123456',
-       '99999999-8888-7777-6666-555555555555',
-       now(),
-       now() + INTERVAL '30 days'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM "refresh_tokens"
-    WHERE id = 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee'
-       OR token = 'sample-refresh-token-123456'
 );
